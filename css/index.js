bd26c6fba56a2e1105e936a34a41d194cd6c72c3
Shayne Max <mayisashayne9@gmail.com>
2025-04-09 15:16:31 +0530

    Grade Calculator

--- css/index.js
+++ css/index.js
@@ -1,30 +1,53 @@
-//Payment Form Using HTML,CSS and JavaScript;Checked Property
-const Subscribe = document.getElementById("Subscribe");
-const Submit = document.getElementById("mySubmit");
-const Visa = document.getElementById("Visa");
-const MasterCard = document.getElementById("MasterCard");
-const PayPal = document.getElementById("PayPal");
-const subtext = document.getElementById("subtext");
-const maintext = document.getElementById("maintext");
+const Submit = document.getElementById("Submit");
+const Input = document.getElementById("scoreInput");
+const Output = document.getElementById("output");
 
+function getGrade(score) {
+    if (score >= 90) {
+        return "A";
+    }
+    else if (score >= 80) {
+        return "B";
+    }
+    if (score >= 70) {
+        return "C";
+    }
+    else if (score >= 60) {
+        return 'D';
+    }
+    else {
+        return "F";
+    }
+
+}
 Submit.onclick = function () {
-    if (Subscribe.checked) {
-        subtext.textContent = `You are Subscribed`;
+    let score = Input.value;
+    score = Number(score);
+
+    if (score < 0 || score > 100 || isNaN(score)) {
+        Output.textContent = "Please enter a value between 0-100!";
+        Output.style.color = "red";
+        return;
+    }
+
+
+    let grade = getGrade(score);
+    Output.textContent = "Your grade is:" + grade;
+    //Aplying Color depending on Grade.
+    if (grade === "A") {
+        Output.style.color = "green";
+        Output.textContent = "Your grade is:" + grade + "👍";
+    }
+    if (grade === "B" || grade === "C") {
+        Output.style.color = "orange";
+        Output.textContent = "Your grade is:" + grade + "😜";
+    }
+    else if (grade === "D") {
+        Output.style.color = "black";
+        Output.textContent = "Your grade is:" + grade + "😢";
     }
     else {
-        subtext.textContent = `You are not Subscribed`;
-    }
-    if (Visa.checked) {
-        maintext.textContent = `You are paying with Visa!`;
-    }
-    else if (MasterCard.checked) {
-        maintext.textContent = `You are paying with MasterCard!`;
-    }
-    else if (PayPal.checked) {
-        maintext.textContent = `You are paying with PayPal`;
-    }
-    else {
-        maintext.textContent = `Select a payment Method!`;
+        Output.style.color = "red";
+        Output.textContent = "Your grade is:" + grade + "🤦‍♂️";
     }
 }
-
