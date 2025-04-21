3cdd9b214ee506672faf32bfc72c22a5660f5f3d
Shayne Max <mayisashayne9@gmail.com>
2025-04-21 18:03:05 +0530

    Emoji Generator

--- css/Project Folder/css/index.js
+++ css/Project Folder/css/index.js
@@ -1,23 +1,56 @@
+//Emoji Generator
+const Secreteword = "Banana";
+let attempts = 0;
+let guess;
+const Hint = "🍌It is something which you can peel";
+running = true;
 
-const myRoll = document.getElementById("myRoll");
-const result = document.getElementById("result");
-const imageresult = document.getElementById("image-result");
-const values = [];
-const images = [];
+
+while (running) {
+    guess = window.prompt(`Enter a Secreteword to access this website`);
 
 
-myRoll.onclick = function () {
-    const diceNumber = document.getElementById("diceNumber").value;
-    for (let i = 0; i < diceNumber; i++) {
-        const value = Math.floor(Math.random() * 6) + 1;
-        values.push(value);
-        images.push(`<img src=css/${value}.png>`);
 
+    if (guess === null) {
+        window.alert("Game Cancelled!");
+        break;
     }
-    result.textContent = `Dice:${values.join(", ")}`;
-    imageresult.innerHTML = images.join("");
+    if (!isNaN(guess)) {
+        window.alert("Please enter a valid input");
+        continue;
+    }
+
+    if (guess === Secreteword) {
+        window.alert(`Congratulations🥳🥳.The secreteword was ${Secreteword}`);
+        running = false;
+    }
+    else {
+        attempts++;
+        if (attempts > 3) {
+            window.alert(`Wrong answer.Hint:${Hint}`);
+        }
+    }
+
+
+
 }
+//Emoji Generator;
 
 
+const myButton = document.getElementById("myButton");
+const emojiresult = document.getElementById("emoji-result");
+let values = [];
+let images = [];
+
+myButton.onclick = function () {
+    const emojiNumber = document.getElementById("emojiNumber").value;
+    for (let i = 0; i < emojiNumber; i++) {
+        let value = Math.floor(Math.random() * 8) + 1;
+        values.push(value);
+        images.push(`<img src=css/emoji${value}.png>`);
+
+        emojiresult.innerHTML = images.join("");
+    }
 
 
+}
