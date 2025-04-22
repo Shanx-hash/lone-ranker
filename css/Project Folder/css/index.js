a282880093866099616cc4eba097449853f05b37
Shayne Max <mayisashayne9@gmail.com>
2025-04-22 09:26:20 +0530

    MineCraft Emoji Generator

--- css/Project Folder/css/index.js
+++ css/Project Folder/css/index.js
@@ -1,55 +1,16 @@
-//Emoji Generator
-const Secreteword = "Banana";
-let attempts = 0;
-let guess;
-const Hint = "🍌It is something which you can peel";
-running = true;
+//MineCraft Emoji Generator;
+const mySubmit = document.getElementById("mySubmit");
+const EmojiResult = document.getElementById("Emoji-Result");
+const values = [];
+const images = [];
 
+mySubmit.onclick = function () {
+    for (let i = 0; i < 2; i++) {
+        let value = Math.floor(Math.random() * 10) + 1;
 
-while (running) {
-    guess = window.prompt(`Enter a Secreteword to access this website`);
+        const image = `<img src=css/mine${value}.png>`;
 
-
-
-    if (guess === null) {
-        window.alert("Game Cancelled!");
-        break;
-    }
-    if (!isNaN(guess)) {
-        window.alert("Please enter a valid input");
-        continue;
-    }
-
-    if (guess === Secreteword) {
-        window.alert(`Congratulations🥳🥳.The secreteword was ${Secreteword}`);
-        running = false;
-    }
-    else {
-        attempts++;
-        if (attempts > 3) {
-            window.alert(`Wrong answer.Hint:${Hint}`);
-        }
-    }
-
-
-
-}
-//Emoji Generator;
-
-
-const myButton = document.getElementById("myButton");
-const emojiresult = document.getElementById("emoji-result");
-let values = [];
-let images = [];
-
-myButton.onclick = function () {
-    const emojiNumber = document.getElementById("emojiNumber").value;
-    for (let i = 0; i < emojiNumber; i++) {
-        let value = Math.floor(Math.random() * 8) + 1;
-        values.push(value);
-        images.push(`<img src=css/emoji${value}.png>`);
-
-        emojiresult.innerHTML = images.join("");
+        EmojiResult.innerHTML = image;
     }
 
 
